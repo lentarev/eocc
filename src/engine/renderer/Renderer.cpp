f3b638eb2642d593204eda404f010d7a636da993
Egor Lentarev <egor.lentarev@yandex.ru>
2026-01-03 20:21:24 +1000

    Renderer

---
+++ src/engine/renderer/Renderer.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Egor Lentarev on 03.01.2026.
+//
+
+#include "Renderer.h"
+
+Renderer::Renderer(/* args */) {}
+
+Renderer::~Renderer() {}
---
+++ src/engine/renderer/Renderer.h
@@ -0,0 +1,16 @@
+//
+// Created by Egor Lentarev on 03.01.2026.
+//
+
+#ifndef EOCC_RENDERER_H
+#define EOCC_RENDERER_H
+
+class Renderer {
+private:
+    /* data */
+public:
+    Renderer(/* args */);
+    ~Renderer();
+};
+
+#endif  // EOCC_RENDERER_H
