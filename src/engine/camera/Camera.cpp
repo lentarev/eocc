0e3d5c9ee3b04f6324cedb7f629f298580dc69ed
Egor Lentarev <egor.lentarev@yandex.ru>
2026-01-13 23:20:56 +1000

    feat(Camera): add Camera

---
+++ src/engine/camera/Camera.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Egor Lentarev on 13.01.2026.
+//
+
+#include "Camera.h"
+
+Camera::Camera() = default;
+
+glm::mat4 Camera::getViewMatrix() const {
+    return glm::lookAt(_position, _target, _up);
+}
+
+glm::mat4 Camera::getProjectionMatrix(float aspectRatio) const {
+    return glm::perspective(_fov, aspectRatio, _near, _far);
+}
--- src/engine/renderer/Renderer.h
+++ src/engine/renderer/Renderer.h
@@ -5,6 +5,7 @@
 #ifndef EOCC_RENDERER_H
 #define EOCC_RENDERER_H
 
+#include <glad/glad.h>
 #include <memory>
 #include <glm/gtc/matrix_transform.hpp>
 #include <engine/assets/material/Material.h>
@@ -22,6 +23,9 @@ public:
 
 private:
     std::unique_ptr<Shader> _shader;
+
+    // Shader programs
+    GLuint _mainShaderProgram;
 };
 
 #endif  // EOCC_RENDERER_H
