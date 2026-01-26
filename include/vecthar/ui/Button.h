2a5beba060924984d71551734382f836a7c0a722
egor <egor.lentarev@yandex.ru>
2026-01-26 18:56:15 +1000

    feat: UI Button

---
+++ include/vecthar/ui/Button.h
@@ -0,0 +1,38 @@
+//
+// Created by Egor Lentarev on 26.01.2026.
+//
+
+#ifndef VECTHAR_BUTTON_H
+#define VECTHAR_BUTTON_H
+
+#include <string>
+
+namespace vecthar {
+class Renderer;
+}
+
+namespace vecthar::ui {
+
+class Button {
+public:
+    Button(float x, float y, float width, float height, const std::string& text);
+    ~Button();
+
+    bool contains(float mouseX, float mouseY) const;
+
+    void render(Renderer& renderer, float scale = 3.0f) const;
+
+    // Getters for debugging (optional)
+    float getX() const;
+    float getY() const;
+    float getWidth() const;
+    float getHeight() const;
+
+private:
+    float _x, _y, _width, _height;
+    std::string _text;
+};
+
+}  // namespace vecthar::ui
+
+#endif  // VECTHAR_BUTTON_H
